b7588ecdc2bcb3b61f12126532cf3c393d2c4d4d
mzhl1111 <liu.muz@northeastern.edu>
2025-03-11 13:51:38 -0400

    full pipeline

--- scripts/mistral_experiment_pipeline.py
+++ scripts/mistral_experiment_pipeline.py
@@ -1,7 +1,10 @@
 import asyncio
+import importlib
+import inspect
 import sys
 sys.path.append("..")
 
+from evaluator.base_evaluator import RAGEvaluator
 from execution_pipeline.execution_pipeline import ExecutionPipeline
 
 from dotenv import load_dotenv
@@ -27,13 +30,28 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 DATASET_NAME = "RAGEVALUATION-HJKMY/ragbench_delucionqa_400row_mistake_added"
-OUTPUT_NAME = "RAGEVALUATION-HJKMY/Mistral_ragbench_delucionqa_400row_mistake_added-1"
+OUTPUT_NAME = "RAGEVALUATION-HJKMY/Mistral_ragbench_delucionqa_400row_mistake"
 
-from evaluator.evaluators import *
+def get_evaluator_classes():
+    """Retrieve all implemented evaluators derived from RAGEvaluator."""
+    module = importlib.import_module('evaluator.evaluators')
+    evaluator_classes = []
+
+    for _, cls in inspect.getmembers(module, inspect.isclass):
+        if (issubclass(cls, RAGEvaluator) and
+                cls.__module__ == module.__name__ and
+                cls.__name__.endswith('Evaluator') and
+                cls is not RAGEvaluator):
+            evaluator_classes.append(cls)
+
+    return evaluator_classes
+
+CLASSES = get_evaluator_classes()
+logger.fatal(' '.join([i.__name__ for i in CLASSES]))
 
 async def main():
     logger.info("Start processing pipeline")
-    pipeline = ExecutionPipeline([RefusalAccuracyEvaluator, AnswerSimilarityEvaluator, FactualAccuracyEvaluator])
+    pipeline = ExecutionPipeline(get_evaluator_classes())
     res = await pipeline.run_pipeline(dataset_name=DATASET_NAME, save_path="./tmp_data", upload_to_hub=True,
                                       repo_id=OUTPUT_NAME,
                                       model = "mistralai/Ministral-8B-Instruct-2410", base_url = "http://127.0.0.1:30000/v1")
